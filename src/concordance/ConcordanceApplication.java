5c32d2bf92572e805ba7814db37f850d64eeddac
Minh Hoang TCU <minh.hoang@tcu.edu>
2015-10-21 19:17:55 -0500

    Added documentation

--- src/concordance/ConcordanceApplication.java
+++ src/concordance/ConcordanceApplication.java
@@ -1,6 +1,17 @@
 package concordance;
 import concordance.gui.View;
 
+/*
+ * Name: Minh Hoang
+ * Program: Concordance Application
+ * 
+ * Purpose: This program create an alphabetical list of the words
+ *  (especially the important ones) present in a text, usually with citations of the passages concerned.
+ *  
+ * Features:
+ * 	- Nothing. This is only the demo GUI for the program. 
+ */
+
 public class ConcordanceApplication {
 	
 	public static void main(String[] args) {
