4453048c1ae48520ff10866323ea2aa09d7f08d5
Minh Hoang TCU <minh.hoang@tcu.edu>
2015-10-24 21:22:57 -0500

    Tested the newly method (adding with 2 cases)

---
+++ src/concordance/datastructure/TreeTest.java
@@ -0,0 +1,47 @@
+package concordance.datastructure;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TreeTest {
+
+	@Test
+	public void treeAddingTest() {
+		RightThreadedTree tree = new RightThreadedTree();
+		WordNode first = new WordNode("a");
+		WordNode second = new WordNode("c");
+		WordNode thrid = new WordNode("d");
+		WordNode forth = new WordNode("b");
+		WordNode fifth = new WordNode("e");
+//		WordNode sixth = new WordNode("6");
+//		WordNode seventh = new WordNode("7");
+		tree.put(first);
+		tree.put(second);
+		tree.put(thrid);
+		tree.put(forth);
+		tree.put(fifth);
+	}
+
+	@Test
+	public void treeAddingTest2() {
+		RightThreadedTree tree = new RightThreadedTree();
+		WordNode first = new WordNode("a");
+		WordNode second = new WordNode("y");
+		WordNode thrid = new WordNode("e");
+		WordNode forth = new WordNode("b");
+		WordNode fifth = new WordNode("y");
+		WordNode sixth = new WordNode("q");
+		WordNode seventh = new WordNode("i");
+		WordNode eaith = new WordNode("s");
+		tree.put(first);
+		tree.put(second);
+		tree.put(thrid);
+		tree.put(forth);
+		tree.put(fifth);
+		tree.put(sixth);
+		tree.put(seventh);
+		tree.put(eaith);
+	}
+	
+}
