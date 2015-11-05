2cdf1ad78f01727764772f204a63003c76a452cb
Minh Hoang TCU <minh.hoang@tcu.edu>
2015-11-05 02:00:04 -0600

    Can now display to simple in begin words

--- src/concordance/gui/controllers/ControllerFindWordsBeginWith.java
+++ src/concordance/gui/controllers/ControllerFindWordsBeginWith.java
@@ -22,23 +22,40 @@ public class ControllerFindWordsBeginWith implements ActionListener {
 		int length = begin.length();
 		boolean isFound = false;
 		StringBuilder builder = new StringBuilder();
-		builder.append("<h1 style='text-align: center; margin: 0px'> Starting Words</h1>");
-		builder.append(String.format("<p style='text-align: center; font-style: italic; margin: 5px; font-style: 30px'> with <b>%s</b> <p>", begin));
+		if (model.isSimple()) {
+			builder.append("Starting Words with " + begin + "\n\n");
+		}
+		else {
+			builder.append("<h1 style='text-align: center; margin: 0px'> Starting Words</h1>");
+			builder.append(String.format("<p style='text-align: center; font-style: italic; margin: 5px; font-style: 30px'> with <b>%s</b> <p>", begin));
+		}
+		
 		
 		for (WordNode word: model.getWordNodes()) {
 			int currentWordLength = word.getWord().length();
 			if (length <= currentWordLength) {
 				String currentStartWith = word.getWord().substring(0, length);
 				if (currentStartWith.equals(begin)) {
-					builder.append(DisplayHelper.getWordAndContentHTML(word));
+					if (model.isSimple()) {
+						builder.append(DisplayHelper.getWordAndContentSimple(word));
+					}
+					else {
+						builder.append(DisplayHelper.getWordAndContentHTML(word));
+					}
 					isFound = true;
 				}
 			}
 		}
 		
 		if (isFound) {
-			view.setDisplayFieldHTML(builder.toString());
-			view.setDisplayFieldViewToTop();
+			if (model.isSimple()) {
+				view.setDisplayFieldSimple(builder.toString());
+				view.setSimpleDisplayViewToTop();
+			}
+			else {
+				view.setDisplayFieldHTML(builder.toString());
+				view.setDisplayFieldViewToTop();
+			}
 			view.clearLblFeedback();
 		}
 		else {
